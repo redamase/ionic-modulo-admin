c4a0111ff4fd2d117280a56cf476a6ba7445791f
redamase <82352068+redamase@users.noreply.github.com>
2023-10-23 07:23:16 -0600

    changes

---
+++ administracion.page.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { HttpClient, HttpErrorResponse, HttpHeaders, HttpParams } from '@angular/common/http';
+
+
+
+
+@Component({
+  selector: 'app-consumos',
+  templateUrl: './consumos.page.html',
+  styleUrls: ['./consumos.page.scss'],
+})
+
+export class AdministracionPage implements OnInit {
+ 
+ 
+  constructor() {}
+
+}
