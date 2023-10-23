0f2cdce0a7e6ed4937d1f885e751dc6baea61c5c
redamase <82352068+redamase@users.noreply.github.com>
2023-10-23 07:02:57 -0600

    add routing for module administracion

---
+++ administracion-routing.module.ts
@@ -0,0 +1,16 @@
+// admin-routing.module.ts
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { AdministracionComponent } from './administracion.component';
+
+const routes: Routes = [
+  { path: '', component: AdministracionComponent }, // Define las rutas según tus necesidades
+  // ...
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class AdministracionRoutingModule { }
