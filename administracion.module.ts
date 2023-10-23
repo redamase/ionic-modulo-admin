23f5108a293f74dc223fd4d2e26c3d1f3c0d34b7
redamase <82352068+redamase@users.noreply.github.com>
2023-10-23 08:05:10 -0600

    Cambio de contenido del administrador

--- administracion.module.ts
+++ administracion.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { AdministracionPage } from './administracion.page';
 
 import { AdministracionRoutingModule } from './administracion-routing.module';
+import { AdministracionPage } from './administracion.page';
 
 
 @NgModule({
